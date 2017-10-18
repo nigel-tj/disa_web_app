0f9174fc99fd553a80cb00bd9ca8ae58c6d6f3fb
Nigel <nigel@webbox.co.za>
2017-10-18 17:08:24 +0200

    add devise and nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admins
   resources :conditions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "conditions#index"
 end
