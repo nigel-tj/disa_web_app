3da74f957d1097d7fa8ab0ebf1cc6582619debfe
Nigel <nigel@webbox.co.za>
2017-10-20 16:09:58 +0200

    home page

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   devise_for :admins
   resources :conditions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "conditions#index"
+  root to: "home#index"
 end
