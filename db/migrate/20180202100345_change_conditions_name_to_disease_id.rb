c68b82d6f35ae3c99d46bd3aec533945bbf43c93
Nigel <hypej.10@gmail.com>
2018-02-07 17:17:25 +0200

    fix heroku

--- db/migrate/20180202100345_change_conditions_name_to_disease_id.rb
+++
@@ -1,7 +0,0 @@
-class ChangeConditionsNameToDiseaseId < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :conditions, :name, :disease_id
-    change_column :conditions, :disease_id, :integer
-    
-  end
-end
