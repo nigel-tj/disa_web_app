3b59dbfb3558022b22ca3461cbca6be805a4ea05
Nigel <hypej.10@gmail.com>
2018-02-07 17:09:46 +0200

    fix heroku col change

--- db/migrate/20180202100345_change_conditions_name_to_disease_id.rb
+++ db/migrate/20180202100345_change_conditions_name_to_disease_id.rb
@@ -1,7 +1,7 @@
 class ChangeConditionsNameToDiseaseId < ActiveRecord::Migration[5.1]
   def change
     rename_column :conditions, :name, :disease_id
-    change_column :conditions, :name, 'integer USING CAST(disease_id AS integer)'
+    change_column :conditions, :disease_id, :integer
     
   end
 end
